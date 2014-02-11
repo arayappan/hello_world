6072ef5274f7ae539f7a707018fb9b3540b26a79
arayappan82 <arayappan@gmail.com>
2014-02-11 11:32:05 +0530

    new line added to 1.cc by hello world2

--- 1.cc
+++ 1.cc
@@ -5,4 +5,5 @@ int main()
 	std::cout << "New cout added in 1.cc" << std::endl;
 	std::cout << "Second cout added in 1.cc" << std::endl;
 	std::cout << "Third cout added in 1.cc" << std::endl;
+	std::cout << "Fourth line added by Hello world2" << std::endl;
 }
